bab86ddf59950ab4fef9c4a3efa8ebf7e3e4ce1b
Alex <alex.adaumi@gmail.com>
2020-01-02 17:34:04 -0300

    commit studys from november 2019

---
+++ huntweb/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./styles.css";
+
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
