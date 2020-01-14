2c6d982d5efd5520ef5e2dcdfde3772e1e70a279
Alex <alex.adaumi@gmail.com>
2020-01-14 19:01:44 -0300

    git compare

--- node/apimarketplace/src/app/controllers/AdController.js
+++ node/apimarketplace/src/app/controllers/AdController.js
@@ -1,58 +1,60 @@
-const Ad = require('../models/Ad')
+const Ad = require("../models/Ad");
 
 class AdController {
   async index(req, res) {
-    const filters = {}
+    const filters = {};
 
     if (req.query.price_min || req.query.pric_max) {
-      filters.price = {}
+      filters.price = {};
 
       if (req.query.price_min) {
-        filters.price.$gte = req.query.price_min
+        filters.price.$gte = req.query.price_min;
       }
 
       if (req.query.price_max) {
-        filters.price.$gte = req.query.price_max
+        filters.price.$gte = req.query.price_max;
       }
 
       if (req.query.title) {
-        filters.title = new RegExp(req.query.title, 'i')
+        filters.title = new RegExp(req.query.title, "i");
       }
     }
 
     const ads = await Ad.paginate(filters, {
       page: req.query.page || 1,
       limit: 20,
-      populate: ['author'],
-      sort: '-createdAt'
-    })
+      populate: ["author"],
+      sort: "-createdAt"
+    });
 
-    return res.json(ads)
+    return res.json(ads);
   }
 
   async show(req, res) {
-    const ad = await Ad.findById(req.params.id)
+    const ad = await Ad.findById(req.params.id);
 
-    return res.json(ad)
+    return res.json(ad);
   }
 
   async store(req, res) {
-    const ad = await Ad.create({ ...req.body, author: req.userId })
+    const ad = await Ad.create({ ...req.body, author: req.userId });
 
-    return res.json(ad)
+    return res.json(ad);
   }
 
   async update(req, res) {
-    const ad = await Ad.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    const ad = await Ad.findByIdAndUpdate(req.params.id, req.body, {
+      new: true
+    });
 
-    return res.json(ad)
+    return res.json(ad);
   }
 
   async destroy(req, res) {
-    const ad = await Ad.findByIdAndDelete(req.params.id)
+    const ad = await Ad.findByIdAndDelete(req.params.id);
 
-    return res.send()
+    return res.send();
   }
 }
 
-module.exports = new AdController()
+module.exports = new AdController();
