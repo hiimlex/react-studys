7ae95b6f94e8816f4a9182c065c80e975263c627
lex <alex.adaumi@gmail.com>
2020-01-13 23:58:46 -0300

    api market place unfinished

---
+++ node/apimarketplace/src/config/mail.js
@@ -0,0 +1,9 @@
+module.exports = {
+  host: 'smtp.mailtrap.io',
+  post: '2525',
+  secure: false,
+  auth: {
+    user: '0d2a5cc45f52dd',
+    pass: '4e7b5f8ec10085'
+  }
+}
---
+++ node/apimarketplace/src/index.js
@@ -0,0 +1,4 @@
+const server = require('./server')
+
+server.listen(3001 || process.env.PORT)
+
